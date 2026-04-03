b7717c0aeb36d24f5ed584e06b0a783f20b93b8d
Samaira Gupta <samaira.guptains@gmail.com>
2026-04-04 00:17:22 +0530

    Updated UI and Java

--- Q2_MediaPlayer/app/src/main/java/com/example/mediaplayer/MainActivity.java
+++ Q2_MediaPlayer/app/src/main/java/com/example/mediaplayer/MainActivity.java
@@ -1,24 +1,75 @@
 package com.example.mediaplayer;
 
-import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.widget.*;
 
 public class MainActivity extends AppCompatActivity {
+
+    VideoView videoView;
+    MediaPlayer mediaPlayer;
+    EditText urlInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        Button openFile = findViewById(R.id.openFile);
+        Button openUrl = findViewById(R.id.openUrl);
+        Button play = findViewById(R.id.play);
+        Button pause = findViewById(R.id.pause);
+        Button stop = findViewById(R.id.stop);
+        Button restart = findViewById(R.id.restart);
+
+        videoView = findViewById(R.id.videoView);
+        urlInput = findViewById(R.id.urlInput);
+
+        // Open Audio File
+        openFile.setOnClickListener(v -> {
+            Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Audio.Media.EXTERNAL_CONTENT_URI);
+            startActivityForResult(intent, 1);
         });
+
+        // Load Video from URL
+        openUrl.setOnClickListener(v -> {
+            String url = urlInput.getText().toString();
+            videoView.setVideoURI(Uri.parse(url));
+            videoView.requestFocus();
+            Toast.makeText(this, "Video Loaded", Toast.LENGTH_SHORT).show();
+        });
+
+        // Controls
+        play.setOnClickListener(v -> videoView.start());
+
+        pause.setOnClickListener(v -> videoView.pause());
+
+        stop.setOnClickListener(v -> videoView.stopPlayback());
+
+        restart.setOnClickListener(v -> {
+            videoView.seekTo(0);
+            videoView.start();
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 1 && resultCode == RESULT_OK) {
+            Uri audioUri = data.getData();
+
+            if (mediaPlayer != null) {
+                mediaPlayer.release();
+            }
+
+            mediaPlayer = MediaPlayer.create(this, audioUri);
+            mediaPlayer.start();
+        }
     }
 }
